0fbd022f09140b5d5b9e4f339370db3290d486fe
Bhanu <prakashbhanu9550@gmail.com>
2026-09-21 22:09:07 +0530

    update multiple page routes to normal scroling page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Services from "./pages/services/Services";
 import Contact from "./pages/contact/Contact";
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
-    //! Routes 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/services" element={<Services />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <a href="#home"></a>
+
+      <Navbar />
+
+      <main>
+        <section id="home">
+          <Home />
+        </section>
+
+        <section id="about">
+          <About />
+        </section>
+
+        <section id="services">
+          <Services />
+        </section>
+
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+
+      <Footer />
+    </>
   )
 }
 
